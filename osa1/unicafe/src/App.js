bc53ee7a494287ab0fbb1ece52e4c30f60c00310
Lassi Heikkinen <abula@kapsi.fi>
2022-06-16 15:29:22 +0300

    add 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -12,19 +12,21 @@ const Statistics = ({ good, neutral, bad }) => {
   const positive = 100 * (good / all)
 
   return (
-    <div>
-      <StatisticsLine text="good" value={good}/>
-      <StatisticsLine text="neutral" value={neutral}/>
-      <StatisticsLine text="bad" value={bad}/>
-      <StatisticsLine text="all" value={all}/>
-      <StatisticsLine text="average" value={average}/>
-      <StatisticsLine text="positive" value={positive}/>
-    </div>
+    <table>
+      <tbody>
+        <StatisticsLine text="good" value={good}/>
+        <StatisticsLine text="neutral" value={neutral}/>
+        <StatisticsLine text="bad" value={bad}/>
+        <StatisticsLine text="all" value={all}/>
+        <StatisticsLine text="average" value={average}/>
+        <StatisticsLine text="positive" value={positive + ' %'}/>
+      </tbody>
+    </table>
   )
 }
 
 const StatisticsLine = ({ text, value }) => {
-  return <>{text} {value}<br/></>
+  return <tr><td>{text}</td><td>{value}</td></tr>
 }
 
 const App = () => {
